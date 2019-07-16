e6a037ef71779e8497fbf676c569697be9e4fc37
Sanjay Khadka <khadka.sk7@gmail.com>
2019-07-16 12:36:31 +0545

    changes in media database in service provider

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -14,7 +14,7 @@ class MediaServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+//        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/resources/views','media-library');
         $this->publishes([
             __DIR__.'/database/migrations' => database_path('migrations')
